d7aa3f1744d2b105846fae04254e70608f186f64
IreneMMM <menshovaiv@gmail.com>
2021-02-03 00:01:56 +0200

    Функции_исправлено

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Этот пример с mdn  - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 const getRandomIntInclusive = (min, max) => {
@@ -5,16 +7,12 @@ const getRandomIntInclusive = (min, max) => {
   max = Math.floor(max);
   if (min < 0 || max < 0) {
     return 'Диапазон указан неверно. Значения не должны быть отрицательными.';
-  }
-  else if (min == max || min > max) {
+  } else if (min >= max) {
     return 'Диапазон указан неверно. Минимальное значение должно быть меньше максимального.';
-  }
-  else {
+  } else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
-
-getRandomIntInclusive(1, 8);
 
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
@@ -22,13 +20,13 @@ getRandomIntInclusive(1, 8);
 const getRandomFloat = (min, max, numbersAfter) => {
   if (min < 0 || max < 0) {
     return 'Диапазон указан неверно. Значения не должны быть отрицательными.';
-  }
-  else if (min == max || min > max) {
+  } else if (min >= max) {
     return 'Диапазон указан неверно. Минимальное значение должно быть меньше максимального.';
-  }
-  else {
+  } else {
     return (Math.random() * (max - min) + min).toFixed(numbersAfter);
   }
 }
 
-getRandomFloat(1.0001, 1.0008, 5);
+
+getRandomIntInclusive();
+getRandomFloat();
